02b2c279b797537838e019c594f438d83a65a92e
lucas.s.passos <lucas.s.passos@stf.jus.br>
2022-10-06 03:55:27 -0300

    Agora depois de confimar a operação escolhida, a calculadora exibe dois campos para input de valores e um botao para confimar a operação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('#formulario');
 
+//Recebe e valida a Operação selecionada
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const inputOperacao = e.target.querySelector('#operacao');
@@ -17,26 +18,32 @@ form.addEventListener('submit', function(e){
     }
     let operacaonome;
 
+//Retorna a Operação selecionada
+
     if(operacao==1) {
         let operacaonome = 'Adição';
         const msg = `Sua operação é ${operacaonome}`;
         setResultado(msg, true);
+        setOperacao(operacao);
     } 
 
     if(operacao==2) {
         let operacaonome = 'Subtração'; 
         const msg = `Sua operação é ${operacaonome}`;
         setResultado(msg, true);
+        setOperacao(operacao);
     } 
     if(operacao==3) {
         let operacaonome = 'Multiplicação'; 
         const msg = `Sua operação é ${operacaonome}`;
         setResultado(msg, true);
+        setOperacao(operacao);
     }  
     if(operacao==4) {
         let operacaonome = 'Divisão'; 
         const msg = `Sua operação é ${operacaonome}`;
         setResultado(msg, true);
+        setOperacao(operacao);
     }  
     
 })
@@ -44,6 +51,25 @@ form.addEventListener('submit', function(e){
 function criaP(){
     const p = document.createElement('p');
     return p;
+}
+
+function criaInputs(){
+    const divAtual = document.getElementById("operacaocampos");
+    const ParagrafoNovo = document.createTextNode("Digite os Valores");
+    const Input1 = document.createElement('input');
+    const Input2 = document.createElement('input');
+    const btnconfirma = document.createElement('button');
+    const btntexto = document.createTextNode("Confirmar");
+    btnconfirma.appendChild(btntexto);
+    divAtual.appendChild(ParagrafoNovo);
+    divAtual.appendChild(Input1);
+    divAtual.appendChild(Input2);
+    divAtual.appendChild(btnconfirma);
+}
+
+function setOperacao(operacao){
+    criaInputs();
+   // const Input1 = e.target.querySelector('#operacao');
 }
 
 function setResultado(msg, isValid){
@@ -59,4 +85,5 @@ function setResultado(msg, isValid){
     }
     p.innerHTML = msg;
     resultado.appendChild(p);
+
 }
